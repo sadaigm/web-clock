cb14ed4e7ab300e314e7df39f8f0288344c41344
sadaigm <sadaigm@gmail.com>
2023-01-09 00:02:55 -0500

    section title padding adjustment

--- src/components/screenlayout/Screen.tsx
+++ src/components/screenlayout/Screen.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { screenSize } from "../grid/device";
 import Grid from "../grid/Grid";
 import NewModal from "../modal/NewModal";
 // import AddButton from "./components/AddButton";
@@ -9,7 +10,12 @@ import ScreenProvider from "./ScreenProvider";
 function Screen() {
   return (
     <div>
-      <CenterContainer style={{ color: "crimson" }}>
+      <CenterContainer
+        style={{
+          color: "crimson",
+          padding: `${window.innerWidth < screenSize.laptop ? "1rem" : "2rem"}`,
+        }}
+      >
         <h1>World Web Clock</h1>
       </CenterContainer>
       <ScreenProvider>
